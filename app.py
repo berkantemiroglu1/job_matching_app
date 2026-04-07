3d06706b2172e9f032370cc13f52b5dd2993e8de
Berkant <berkantemiroglu1@gmail.com>
2026-04-07 13:13:39 +0300

    feat: Kullanici kayit (register) API ucu eklendi

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask
-from models import db
+from flask import Flask, request, jsonify
+from models import db, Kullanici
 
 app = Flask(__name__)
-# veritabanı ayarı (Aynı klasöre veritabani.db adında bir dosya açacak)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///veritabani.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-# Uygulama ilk çalıştığında veritabanı tablolarını otomatik oluşturur
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def ana_sayfa():
     return "Sistem Ayakta! Veritabanı Hazır."
+
+@app.route('/kayit', methods=['POST'])
+def kayit_ol():
+    veri = request.get_json()
+    
+    yeni_kullanici = Kullanici(
+        eposta=veri['eposta'],
+        sifre=veri['sifre'],
+        kullanici_tipi=veri['kullanici_tipi']
+    )
+    
+    db.session.add(yeni_kullanici)
+    db.session.commit()
+    
+    return jsonify({"mesaj": "Kullanici basariyla olusturuldu!"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
